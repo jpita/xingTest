485ed90c035cfd70547d0686e41a8c677cdf5807
jpita <dvpita@gmail.com>
2016-03-02 23:37:08 +0100

    trying to destroy objects

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,28 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-	before(:all)do
-		@item = Item.new(name:"the name", description:"the description")
+RSpec.describe Item, type: :model do	
+	before(:all) do
+		@item=Item.create(name:"the name", description:"the description")
 	end
 	
-	it "should have a matching name"do
+	it "should have a matching name and description after creating"do
 		expect(@item.name).to eq("the name")
-	end
-	it "should have a matching description"do
 		expect(@item.description).to eq("the description")
 	end
 
-	it "should have a new matching name"do
-		@item=@item.update(name:"new name", description:"new description")
+	it "should have a new matching name and description after update"do
+		@item.update(name:"new name", description:"new description")
 		expect(@item.name).to eq("new name")
-	end
-	it "should have a new matching updated description"do
 		expect(@item.description).to eq("new description")
 	end
-	
-	it "there should be one item on the list"do
-		expect(Item.all).to eq(1)
-	end   
-	    
+
+	it "the item should be destroyed"do
+		@item.destroy
+		expect(Item.count).to eq(0)
+		@item=nil
+	end    
 
 end
